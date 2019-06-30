e0ee17cd983fffc44e7f15cd5fcbf5193f0fdd32
Riley Sumner <rsumner@wisc.edu>
2019-06-30 17:36:32 -0500

    article template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Bell</h1>
+    <Article title="My Article"
+             byline="Riley Sumner"
+             tagline="This article is about writing"
+             img="logo.png">
+    </Article>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Article from './components/Article.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Article
   }
 }
 </script>
---
+++ src/components/Article.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="article">
+        <div class="article-heading">
+            <h1>{{ title }}</h1>
+            <p class="byline">{{ byline }}</p>
+        </div>
+        <div class="article-body">
+            <p>{{ tagline }}</p>
+            <img :src=img />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Article",
+        props: {
+            title: String,
+            byline: String,
+            tagline: String,
+            img: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
