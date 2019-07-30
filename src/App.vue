842197647342add2a3b57cd77e243ac803903161
Riley Sumner <rsumner@wisc.edu>
2019-07-30 01:40:06 -0500

    font path

--- src/App.vue
+++ src/App.vue
@@ -20,29 +20,29 @@ import Header from "./components/Header";
 </script>
 
 <style lang="scss">
-  @import "/assets/styles/global.scss";
+  @import "assets/styles/global.scss";
 
   @font-face {
     font-family: Lemon/Milk;
-    src: url("src/assets/fonts/LemonMilk.otf") format('opentype');
+    src: url("assets/fonts/LemonMilk.otf") format('opentype');
     font-weight:400;
   }
 
   @font-face {
     font-family: Quicksand;
-    src: url("src/assets/fonts/Quicksand_Book.otf") format('opentype');
+    src: url("assets/fonts/Quicksand_Book.otf") format('opentype');
     font-weight:400;
   }
 
   @font-face {
     font-family: Bebas Kai;
-    src: url("src/assets/fonts/BebasKai.otf") format('opentype');
+    src: url("assets/fonts/BebasKai.otf") format('opentype');
     font-weight:400;
   }
 
   @font-face {
     font-family: Satisfy;
-    src: url("src/assets/fonts/Satisfy-Regular.ttf") format('truetype');
+    src: url("assets/fonts/Satisfy-Regular.ttf") format('truetype');
     font-weight:400;
   }
 </style>
