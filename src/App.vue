17bf49b5d48abde43a49995a67f0024a8d9fd8d4
Riley Sumner <rsumner@wisc.edu>
2019-07-30 12:51:10 -0500

    maybe this will work

--- src/App.vue
+++ src/App.vue
@@ -20,29 +20,25 @@ import Header from "./components/Header";
 </script>
 
 <style lang="scss">
-  @import "assets/styles/global.scss";
-
   @font-face {
-    font-family: Lemon/Milk;
+    font-family: Lemon;
     src: url("assets/fonts/LemonMilk.otf") format('opentype');
-    font-weight:400;
   }
 
   @font-face {
     font-family: Quicksand;
     src: url("assets/fonts/Quicksand_Book.otf") format('opentype');
-    font-weight:400;
   }
 
   @font-face {
     font-family: Bebas Kai;
     src: url("assets/fonts/BebasKai.otf") format('opentype');
-    font-weight:400;
   }
 
   @font-face {
     font-family: Satisfy;
     src: url("assets/fonts/Satisfy-Regular.ttf") format('truetype');
-    font-weight:400;
   }
+  
+  @import "assets/styles/global.scss";
 </style>
