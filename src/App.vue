03986058d15a7725f25376c5f0c10aec44104acb
Riley Sumner <rsumner@wisc.edu>
2019-10-21 16:00:13 -0500

    dark styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app">
+    <div id="app" class="app">
         <Header></Header>
         <div class="content">
             <router-view @authenticated="setAuthenticated" />
